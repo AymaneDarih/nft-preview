ce883a896526508ac043327ad191c18386a66b42
Aymane Darih <73829655+AymaneDarih@users.noreply.github.com>
2021-12-14 09:30:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use:strict";
+const nft = document.querySelector(".nft");
+const nft__img = document.querySelector(".nft__img");
+const html = ` <div class="nft__shadow" id="shadow"></div>`;
+
+nft__img.addEventListener("mouseover", function (e) {
+  console.log("yes baby");
+  e.preventDefault();
+  if (e.target.classList.contains("nft__img")) {
+    nft.insertAdjacentHTML("beforeend", html);
+  }
+});
+
+nft__img.addEventListener("mouseout", removeChild);
+
+function removeChild() {
+  const shadow = document.getElementById("shadow");
+  const parent = shadow.parentNode;
+  parent.removeChild(shadow);
+  console.log("removed");
